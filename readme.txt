18ae2d3710d3bbef175917dfea1e2f24528c402a
MuhammadFahrial <rialjhe111@gmail.com>
2024-02-27 08:30:58 +0800

    first commit

---
+++ readme.txt
@@ -0,0 +1,11 @@
+https://dummyjson.com/users
+
+"Username  : atuny0 "
+"Passoword : 9uQFF1Lh""
+
+"Username  : hbingley1 "
+"Passoword : CQutx25i8r"
+
+"Username  : rshawe2 "
+"Passoword : OWsTbMUgFc"
+
---
+++ script.js
@@ -0,0 +1,56 @@
+const inputUsername = document.getElementById("username");
+const inputPassword = document.getElementById("password");
+const login = document.getElementById("CTA");
+const isLogin = document.querySelector(".isLogin");
+
+const authLogin = async (username, password) => {
+  const url = "https://dummyjson.com/auth/login";
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+      // expiresInMins: 60, // optional
+    }),
+  };
+  try {
+    const res = await fetch(url, options);
+    const result = await res.json();
+    return result.token;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const currentUser = async (username, password) => {
+  const token = await authLogin(username, password);
+  const url = "https://dummyjson.com/auth/me";
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  try {
+    const res = await fetch(url, options);
+    const result = await res.json();
+    const text = `
+    <p>First Name : <span>${result.firstName}</span></p>
+    <p>Last Name : <span>${result.lastName}</span> </p>
+    <p>Age : <span>${result.age}</span></p>
+    <p>Gender : <span>${result.gender}</span></p>
+    <p>Email : <span>${result.email}</span></p>
+    <p>Phone : <span>${result.phone}</span></p>
+    `;
+    isLogin.innerHTML += text;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+login.addEventListener("click", () => {
+  currentUser(inputUsername.value, inputPassword.value);
+});
+
